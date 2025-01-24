1200c02105258290da7c279692a509ebd6ff1372
Marcell <marcellusgerson@gmail.com>
2025-01-25 02:16:52 +0700

    readme

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use(morgan('tiny'))
 
-app.get("/", (request, response) => {
+app.get("", (request, response) => {
   response.send('<h1>ERP API</h1>')
 })
 
